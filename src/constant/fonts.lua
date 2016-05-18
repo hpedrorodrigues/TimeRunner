6879f9cba4b8a593acd899a970ac69318e947212
Pedro Rodrigues <hs.pedro.rodrigues@gmail.com>
2016-05-18 01:47:59 -0300

    Improving fonts

--- src/constant/fonts.lua
+++ src/constant/fonts.lua
@@ -1,11 +1,7 @@
-local BASE_FONT_PATH = ''
-
-local isSimulator = (system.getInfo('environment') == 'simulator')
-
 local fonts = {
-    BOLD = isSimulator and BASE_FONT_PATH .. 'Quicksand-Bold' or 'Quicksand-Bold',
-    BOLD_ITALIC = isSimulator and BASE_FONT_PATH .. 'Quicksand-BoldItalic' or 'Quicksand-BoldItalic',
-    ITALIC = isSimulator and BASE_FONT_PATH .. 'Quicksand-Italic' or 'Quicksand-Italic'
+    BOLD = 'Quicksand-Bold',
+    BOLD_ITALIC = 'Quicksand-BoldItalic',
+    ITALIC = 'Quicksand-Italic'
 }
 
 fonts.SYSTEM = fonts.BOLD_ITALIC
